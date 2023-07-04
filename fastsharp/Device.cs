68ce2bbb31b803bcabc70b3e12efdf3a1e162ef2
chair <projectaa76@gmail.com>
2023-07-03 23:39:41 -0400

    Scaffold Silk and D3D11 context

--- fastsharp/Device.cs
+++ fastsharp/Device.cs
@@ -1,6 +1,86 @@
-﻿namespace FastSharp;
+﻿using FastSharp.Providers;
+using Silk.NET.Core.Native;
+using Silk.NET.Direct3D11;
+using Silk.NET.DXGI;
 
-public class Device
+namespace FastSharp;
+
+public class Device : IDisposable
 {
-    // TODO: things
+    internal ComPtr<ID3D11Device> GraphicsDevice;
+
+    internal ComPtr<ID3D11DeviceContext> GraphicsDeviceContext;
+
+    private bool Disposed;
+
+    public unsafe Device()
+    {
+        SilkMarshal.ThrowHResult
+        (
+            D3D11PRovider.D3D11.CreateDevice
+            (
+                pAdapter: default(ComPtr<IDXGIAdapter>),
+                DriverType: D3DDriverType.Hardware,
+                Software: 0,
+#if DEBUG
+                Flags: (uint)CreateDeviceFlag.Debug,
+#else
+                Flags: (uint)CreateDeviceFlag.None,
+#endif
+                pFeatureLevels: null,
+                FeatureLevels: 0,
+                SDKVersion: D3D11.SdkVersion,
+                ppDevice: ref GraphicsDevice,
+                pFeatureLevel: null,
+                ppImmediateContext: ref GraphicsDeviceContext
+            )
+        );
+
+#if DEBUG
+        GraphicsDevice.SetInfoQueueCallback(x =>
+        {
+            switch (x.Severity)
+            {
+                case MessageSeverity.Corruption:
+                case MessageSeverity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case MessageSeverity.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case MessageSeverity.Info:
+                case MessageSeverity.Message:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+            }
+
+            Console.WriteLine($"[{x.Severity}] {SilkMarshal.PtrToString((nint)x.PDescription)}");
+        });
+#endif
+
+
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!Disposed)
+        {
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects)
+            }
+
+            GraphicsDeviceContext.Dispose();
+
+            GraphicsDevice.Dispose();
+
+            Disposed = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
 }
---
+++ fastsharp/Providers/D3D11PRovider.cs
@@ -0,0 +1,16 @@
+﻿using Silk.NET.Direct3D11;
+using Silk.NET.Windowing;
+
+namespace FastSharp.Providers;
+
+internal static class D3D11PRovider
+{
+    public static D3D11 D3D11 { get; private set; }
+
+    static D3D11PRovider()
+    {
+#pragma warning disable CS0618 // Type or member is obsolete
+        D3D11 = D3D11.GetApi();
+#pragma warning restore CS0618
+    }
+}
