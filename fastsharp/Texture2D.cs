d330eca23e13882ec6fbcb47632c2993ba081de5
chair <projectaa76@gmail.com>
2023-07-04 21:05:16 -0400

    Refactor `Texture2D` description fields a bit

--- fastsharp/Texture2D.cs
+++ fastsharp/Texture2D.cs
@@ -12,19 +12,17 @@ public unsafe class Texture2D : Texture<ID3D11Texture2D>
 
     protected override int Size => Width * Height;
 
-    internal Texture2D(Device device)
+    internal Texture2D(Device device, Texture2DDesc desc)
         : base(device)
     {
+        SilkMarshal.ThrowHResult(Device.GraphicsDevice.CreateTexture2D(desc, (SubresourceData*)null, ref GraphicsTexture));
 
+        CacheDescriptionFields(desc);
     }
 
     public Texture2D(Device device, int width, int height)
         : base(device)
     {
-        Width = width;
-
-        Height = height;
-
         Texture2DDesc desc = new Texture2DDesc()
         {
             Width = (uint)width,
@@ -36,18 +34,14 @@ public unsafe class Texture2D : Texture<ID3D11Texture2D>
             Format = Format.FormatR8G8B8A8Unorm,
         };
 
-        Format = desc.Format;
-
         SilkMarshal.ThrowHResult(Device.GraphicsDevice.CreateTexture2D(desc, (SubresourceData*)null, ref GraphicsTexture));
+
+        CacheDescriptionFields(desc);
     }
 
     public Texture2D(Device device, int width, int height, Format format)
         : base(device)
     {
-        Width = width;
-
-        Height = height;
-
         Texture2DDesc desc = new Texture2DDesc()
         {
             Width = (uint)width,
@@ -59,9 +53,9 @@ public unsafe class Texture2D : Texture<ID3D11Texture2D>
             Format = format
         };
 
-        Format = desc.Format;
-
         SilkMarshal.ThrowHResult(Device.GraphicsDevice.CreateTexture2D(desc, (SubresourceData*)null, ref GraphicsTexture));
+
+        CacheDescriptionFields(desc);
     }
 
     internal Texture2DDesc GetTextureDescription()
@@ -73,17 +67,20 @@ public unsafe class Texture2D : Texture<ID3D11Texture2D>
         return textureDesc;
     }
 
+    private void CacheDescriptionFields(Texture2DDesc? textureDesc = null)
+    {
+        Texture2DDesc desc = textureDesc ?? GetTextureDescription();
+
+        Width = (int)desc.Width;
+
+        Height = (int)desc.Height;
+
+        Format = desc.Format;
+    }
+
     internal Texture2D CreateStagingTexture()
     {
-        Texture2D stagingTexture = new Texture2D(Device);
-
         Texture2DDesc desc = GetTextureDescription();
-
-        stagingTexture.Width = Width;
-
-        stagingTexture.Height = Height;
-
-        stagingTexture.Format = Format;
 
         desc = desc with
         {
@@ -94,8 +91,7 @@ public unsafe class Texture2D : Texture<ID3D11Texture2D>
             SampleDesc = new SampleDesc(1, 0)
         };
 
-        SilkMarshal.ThrowHResult(Device.GraphicsDevice.CreateTexture2D(desc, (SubresourceData*)null, ref stagingTexture.GraphicsTexture));
 
-        return stagingTexture;
+        return new Texture2D(Device, desc);
     }
 }
