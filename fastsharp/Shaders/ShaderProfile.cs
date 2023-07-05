8d61eb862f94bd244269d9985401493592b1f3f7
chair <projectaa76@gmail.com>
2023-07-05 17:50:26 -0400

    Remove 6.0 shader profiles

--- fastsharp/Shaders/ShaderProfile.cs
+++ fastsharp/Shaders/ShaderProfile.cs
@@ -8,14 +8,6 @@ public class ShaderProfile
 
     public static readonly ShaderProfile VS5_0 = new ShaderProfile("vs_5_0");
 
-
-    public static readonly ShaderProfile CS6_0 = new ShaderProfile("cs_6_0");
-
-    public static readonly ShaderProfile PS6_0 = new ShaderProfile("ps_6_0");
-
-    public static readonly ShaderProfile VS6_0 = new ShaderProfile("vs_6_0");
-
-
     private readonly string Profile;
 
     private ShaderProfile(string profile)
