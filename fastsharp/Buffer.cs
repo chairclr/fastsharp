32ff44c092940a050a61ac28e466e6c3964c4d61
chair <projectaa76@gmail.com>
2023-07-05 17:43:13 -0400

    Add `Buffer` base class

---
+++ fastsharp/Buffer.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using Silk.NET.Core.Native;
+using Silk.NET.Direct3D11;
+
+namespace FastSharp;
+
+public abstract class Buffer : IDisposable
+{
+    public readonly Device Device;
+
+    internal ComPtr<ID3D11Buffer> GraphicsBuffer = default;
+
+    public virtual uint Length { get; protected set; } = 0;
+
+    private bool Disposed;
+
+    public Buffer(Device device)
+    {
+        Device = device;
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!Disposed)
+        {
+
+
+            Disposed = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
+
+public abstract class Buffer<T> : Buffer
+{
+    public uint Stride => (uint)Unsafe.SizeOf<T>();
+
+    public uint Size => Stride * Length;
+
+    protected Buffer(Device device)
+        : base(device)
+    {
+
+    }
+}
