71ac10416dae9a6cd07cecbe712f465a53b32ba2
chair <projectaa76@gmail.com>
2023-07-06 14:24:12 -0400

    Add `CreateStructuredBuffer` and `CreateRWStructuredBuffer` methods to `Device`

--- fastsharp/Device.Buffers.cs
+++ fastsharp/Device.Buffers.cs
@@ -15,4 +15,28 @@ public partial class Device
     {
         return new ConstantBuffer<T>(this, initialData);
     }
+
+    public StructuredBuffer<T> CreateStructuredBuffer<T>(int length, bool cpuReadable = false, bool cpuWritable = false)
+        where T : unmanaged
+    {
+        return new StructuredBuffer<T>(this, length, cpuReadable, cpuWritable);
+    }
+
+    public StructuredBuffer<T> CreateStructuredBuffer<T>(ReadOnlySpan<T> initialData, bool cpuReadable = false, bool cpuWritable = false)
+        where T : unmanaged
+    {
+        return new StructuredBuffer<T>(this, initialData, cpuReadable, cpuWritable);
+    }
+
+    public RWStructuredBuffer<T> CreateRWStructuredBuffer<T>(int length, bool cpuReadable = false, bool cpuWritable = false)
+        where T : unmanaged
+    {
+        return new RWStructuredBuffer<T>(this, length, cpuReadable, cpuWritable);
+    }
+
+    public RWStructuredBuffer<T> CreateRWStructuredBuffer<T>(ReadOnlySpan<T> initialData, bool cpuReadable = false, bool cpuWritable = false)
+        where T : unmanaged
+    {
+        return new RWStructuredBuffer<T>(this, initialData, cpuReadable, cpuWritable);
+    }
 }
